9a9459e172da764683c6553c782a715dd22a069e
Scott Walter <scott.walter@nice.com>
2024-04-15 13:27:57 -0500

    first draft of automated setup

--- bin/bin/run-stow.sh
+++ bin/bin/run-stow.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env /opt/homebrew/bin/bash
 #!/usr/bin/env /bin/zsh
 
-stow -d ~/dotfiles -t ~/ bash bin config csh duckdns finicky git mysetup prettier sh ssh tcsh tmux vim zsh
+stow -d ~/dotfiles -t ~/ bash bin config csh duckdns finicky git setup prettier sh ssh tcsh tmux vim zsh
 
--- setup/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-DOTFILES_DIR="$HOME/Dropbox/.dotfiles"
-SSH_DIR="$HOME/.ssh"
-
-if ! [ -x "$(command -v ansible)" ]; then
- brew install ansible
-fi
