468d78a486594743e2a2068f8e2462fd34f18e13
Scott Walter <scott.walter@nice.com>
2024-05-23 17:56:32 -0500

    boo

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -8,8 +8,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Navigate vim panes better
-vim.keymap.set('n','<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n','<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n','<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n','<c-l>', ':wincmd l<CR>')
-
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
