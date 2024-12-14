0a02f6a4fc6cd3c0a3f692c9360401fa4b24fc72
Scott Walter <scott.walter@nice.com>
2024-12-13 21:17:14 -0600

    boo

--- setup/init.sh
+++ setup/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/homebrew/bin/stow -d ~/dotfiles -t ~/ bash bin config csh duckdns finicky git prettier sh ssh tcsh tmux vim zsh
+/opt/homebrew/bin/stow -d ~/dotfiles -t ~/ bash bin brew config csh duckdns finicky git prettier sh ssh tcsh tmux vim zsh
