3027e9caaa628dc765676f76a8d3d1b5c354a5eb
Scott Walter <scott@scottwalter.com>
2025-02-12 14:43:42 -0600

    tagged

--- bin/bin/cp-tagged-files.sh
+++ bin/bin/cp-tagged-files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Create the destination directory if it doesn't exist
 
 if [ -z "$1" ]; then
@@ -15,14 +14,8 @@ fi
 mkdir -p $2
 
 input_string="$1"
-#IFS=' ' read -ra words <<<"$input_string"
 read -ra arr <<<"$input_string"
-declare -a processed_strings
 for word in "${arr[@]}"; do
-  echo "wtf: $word"
-  # Create the formatted string and add it to the array
-  echo "kMDItemUserTags == '$word'"
-
   if [[ -z $combined ]]; then
     combined+="kMDItemUserTags == '$word'"
   else
@@ -31,12 +24,9 @@ for word in "${arr[@]}"; do
 
 done
 
-echo $combined
-
-mdfind "$combined" |
-  xargs -n 1 -L 1 -t -I {} cp "{}" ~/favs/
+mdfind "$combined" | xargs -n 1 -L 1 -t -I {} cp "{}" $2
+#mdfind "$combined" | xargs -n 1 -L 1 -t -I {} cp "{}" $2 2>/dev/null
 
 find $2 -type f -print0 | while IFS= read -r -d '' file; do
   xattr -w com.apple.metadata:_kMDItemUserTags '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><array/></plist>' "$file"
-  echo "Removed tags from: $file"
 done
