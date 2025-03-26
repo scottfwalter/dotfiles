9f9e36caaf2195f3a17cb91a960dd03cd0bc5ce4
Scott Walter <scott@scottwalter.com>
2025-03-26 15:46:24 -0500

    git gold

--- config/.config/nvim/lua/plugins/snacks.lua
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -2,15 +2,15 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    lazygit = {
-      -- your lazygit configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
+--    lazygit = {
+--      -- your lazygit configuration comes here
+--      -- or leave it empty to use the default settings
+--      -- refer to the configuration section below
+--    },
     dashboard = {
 }
   },
   keys = {
-     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" }
+--     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" }
   }
 }
