1deef391f7e2ecd372b83cb660138c57e738b0b7
Scott Walter <scott@scottwalter.com>
2025-05-01 14:09:33 -0500

    hello

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -7,15 +7,38 @@ return {
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      -- Custom action to open all selected files in tabs
+      local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
+
+      local function open_multiple_files(prompt_bufnr)
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local selections = picker:get_multi_selection()
+
+        actions.close(prompt_bufnr)
+
+        for _, entry in ipairs(selections) do
+          vim.cmd("edit " .. entry.path)
+        end
+      end
+
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-o>"] = open_multiple_files, -- Ctrl+o opens selected files
+            },
+          },
+        },
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
