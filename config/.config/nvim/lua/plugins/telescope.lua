4da006a536e57f787d3d8362b5ca331c98881a95
Scott Walter <scott@scottwalter.com>
2025-05-07 15:01:54 -0500

    fun

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -29,15 +29,42 @@ return {
           },
         },
         defaults = {
+          --file_ignore_patterns = { "vendor", "node_modules", ".git/" }, -- Ignore node_modules and .git folders
+          --find_command = { "fd", "--type", "f", "--no-ignore", "--hidden" },
           mappings = {
             i = {
               ["<C-o>"] = open_multiple_files, -- Ctrl+o opens selected files
             },
           },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--no-ignore", -- **This is the added flag**
+            "--hidden", -- **Also this flag. The combination of the two is the same as `-uu`**
+          },
+        },
+        pickers = {
+          find_files = {
+            --find_command = { "fd", "--type", "f", "--no-ignore", "--hidden" },
+            hidden = true,
+            no_ignore = true,
+          },
         },
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      -- vim.api.nvim_set_keymap(
+      --   "n",
+      --   "<C-p>",
+      --   ':lua require"telescope.builtin".find_files({ hidden = true, no_ignore = false })<CR>',
+      --   { noremap = true, silent = true }
+      -- )
+
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
