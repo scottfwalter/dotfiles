8a3c5b96b1128448477203f0f3075b406f32e3cd
Scott Walter <scott@scottwalter.com>
2025-05-06 19:23:00 -0500

    copilot

--- config/.config/nvim/lua/plugins/noice.lua
+++ config/.config/nvim/lua/plugins/noice.lua
@@ -1,40 +1,40 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-		lsp = {
-			-- Enables LSP progress, messages, etc.
-			message = {
-				enabled = true,
-			},
-		},
-		messages = {
-			enabled = true, -- enables the message history UI
-			view_search = false,
-		},
-		presets = {
-			command_palette = true,
-			long_message_to_split = true,
-			lsp_doc_border = true,
-		},
-		routes = {
-			{
-				filter = {
-					event = "msg_show",
-					kind = "",
-					find = "@recording",
-				},
-				opts = { skip = false },
-			},
-		},
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+    lsp = {
+      -- Enables LSP progress, messages, etc.
+      message = {
+        enabled = true,
+      },
+    },
+    messages = {
+      enabled = true, -- enables the message history UI
+      view_search = false,
+    },
+    presets = {
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "@recording",
+        },
+        opts = { skip = false },
+      },
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
 }
--- config/.config/nvim/lua/vim-options.lua
+++ config/.config/nvim/lua/vim-options.lua
@@ -50,3 +50,10 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
 	end,
 })
+
+vim.api.nvim_set_keymap("c", "<Down>", 'v:lua.get_wildmenu_key("<right>", "<down>")', { expr = true })
+vim.api.nvim_set_keymap("c", "<Up>", 'v:lua.get_wildmenu_key("<left>", "<up>")', { expr = true })
+
+function _G.get_wildmenu_key(key_wildmenu, key_regular)
+	return vim.fn.wildmenumode() ~= 0 and key_wildmenu or key_regular
+end
