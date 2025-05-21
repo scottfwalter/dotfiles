96fcff397693d69b9f0e7b7166d3fcfcad1a3bcc
Scott Walter <scott@scottwalter.com>
2025-05-21 06:47:27 -0500

    fun fun

--- config/.config/nvim/lua/vim-options.lua
+++ config/.config/nvim/lua/vim-options.lua
@@ -9,6 +9,7 @@ vim.opt.swapfile = false
 vim.wo.number = true
 vim.opt.splitright = true
 vim.opt.relativenumber = true
+vim.opt.hlsearch = true
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
@@ -17,7 +18,7 @@ vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Remove search highlighting
-vim.keymap.set("n", "<F3>", ":nohlsearch<CR>")
+vim.keymap.set("n", "<F3>", ":set hlsearch!<CR>")
 
 --Move lines
 vim.keymap.set("n", "<A-k>", ":m-2<CR>")
@@ -30,39 +31,38 @@ vim.keymap.set("n", "<leader>o", ":!open %<CR>")
 
 -- Dismiss noice message panel
 vim.keymap.set("n", "<Esc>", function()
-  vim.cmd("Noice dismiss")
+	vim.cmd("Noice dismiss")
 end, { desc = "Dismiss Noice messages" })
 
 -- Use arrow keys inside of choice lists instead of tab
 vim.api.nvim_set_keymap("c", "<Down>", 'v:lua.get_wildmenu_key("<right>", "<down>")', { expr = true })
 vim.api.nvim_set_keymap("c", "<Up>", 'v:lua.get_wildmenu_key("<left>", "<up>")', { expr = true })
 function _G.get_wildmenu_key(key_wildmenu, key_regular)
-  return vim.fn.wildmenumode() ~= 0 and key_wildmenu or key_regular
+	return vim.fn.wildmenumode() ~= 0 and key_wildmenu or key_regular
 end
 
 -- Toggle virtual text hints
 vim.api.nvim_create_user_command("ToggleHints", function()
-  local current = vim.diagnostic.config().virtual_text
-  vim.diagnostic.config({ virtual_text = not current })
-  print("Virtual text: " .. (not current and "enabled" or "disabled"))
+	local current = vim.diagnostic.config().virtual_text
+	vim.diagnostic.config({ virtual_text = not current })
+	print("Virtual text: " .. (not current and "enabled" or "disabled"))
 end, {})
 
 -- Add line movements to jump list
 vim.api.nvim_create_augroup("cursor_mark", { clear = true })
 vim.api.nvim_create_autocmd("CursorHold", {
-  group = "cursor_mark",
-  callback = function()
-    vim.cmd("normal! m'")
-  end,
-  desc = "Mark current position when cursor is held",
+	group = "cursor_mark",
+	callback = function()
+		vim.cmd("normal! m'")
+	end,
+	desc = "Mark current position when cursor is held",
 })
 
 vim.diagnostic.config({
-  virtual_text = false,
-  --virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
+	virtual_text = false,
+	--virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = false,
 })
-
