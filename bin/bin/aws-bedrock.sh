527bf4a32cf194f4067c1b7f9892e61d2c2810fd
Scott Walter <scott@scottwalter.com>
2025-07-29 20:27:58 -0500

    bedrock

---
+++ bin/bin/aws-bedrock.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+export AWS_BEARER_TOKEN_BEDROCK=ABSKQmVkcm9ja0FQSUtleS1ydDB1LWF0LTkzNDEzNzEzMjYwMTpWWnl3NkNISGNuRjZKOFJKTzErTkdnUld1b1VDdDlaOUhOS1NhMGJObVN3dER2dUNBVzZnSHMwMllJZz0=
+export CLAUDE_CODE_USE_BEDROCK=1
+export AWS_REGION=us-west-2
+export ANTHROPIC_MODEL='us.anthropic.claude-sonnet-4-20250514-v1:0'
+#export ANTHROPIC_MODEL='us.anthropic.claude-opus-4-20250514-v1:0'
