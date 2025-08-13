79b607e767c7dbc9790536b4829aaf5f737b291b
Scott Walter <scott@scottwalter.com>
2025-08-13 16:02:01 -0500

    boo

--- config/.config/nvim/lua/plugins/claudecode.lua
+++ config/.config/nvim/lua/plugins/claudecode.lua
@@ -12,8 +12,8 @@ return {
 	"coder/claudecode.nvim",
 	dependencies = { "folke/snacks.nvim" },
 	opts = {
-		--terminal_cmd = "~/.claude/local/claude", -- Point to local installation
-		terminal_cmd = "export AWS_BEARER_TOKEN_BEDROCK1= && export CLAUDE_CODE_USE_BEDROCK=1 && export AWS_REGION=us-west-2 && export ANTHROPIC_MODEL='us.anthropic.claude-sonnet-4-20250514-v1:0' && ~/.claude/local/claude", -- Point to local installation
+		terminal_cmd = "~/.claude/local/claude", -- Point to local installation
+		--terminal_cmd = "export AWS_BEARER_TOKEN_BEDROCK1= && export CLAUDE_CODE_USE_BEDROCK=1 && export AWS_REGION=us-west-2 && export ANTHROPIC_MODEL='us.anthropic.claude-sonnet-4-20250514-v1:0' && ~/.claude/local/claude", -- Point to local installation
 	},
 	config = true,
 	keys = {
